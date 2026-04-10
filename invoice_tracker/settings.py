ec93ce25782fa714934d8d5f060c6a7f16b9705d
Ben John <benjohn.mec@gmail.com>
2026-04-10 15:28:00 +0530

    done

--- invoice_tracker/settings.py
+++ invoice_tracker/settings.py
@@ -125,3 +125,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
